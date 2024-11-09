f9c6f48ab8237eb2379517f36f4da8ec03bf571d
IgorFPonchielli <igor.ponchielli@gmail.com>
2024-11-09 17:31:40 -0300

    first version landing-page

---
+++ js/main.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+    // Navbar scroll effect
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 50) {
+            $('.navbar').addClass('scrolled');
+            $('.navbar').removeClass('navbar-dark');
+            $('.nav-link').addClass('scrolled');
+        } else {
+            $('.navbar').removeClass('scrolled');
+            $('.navbar').addClass('navbar-dark');
+            $('.nav-link').removeClass('scrolled');
+
+        }
+    });
+
+    // Smooth scroll for navigation links
+    $('a.nav-link').click(function(e) {
+        e.preventDefault();
+        var target = $($(this).attr('href'));
+        if (target.length) {
+            $('html, body').animate({
+                scrollTop: target.offset().top - 70
+            }, 1000);
+        }
+    });
+
+    // Form submission
+    $('#subscribe-form').submit(function(e) {
+        e.preventDefault();
+        // Add your form submission logic here
+        alert('Formulário enviado com sucesso!');
+        this.reset();
+    });
+});
